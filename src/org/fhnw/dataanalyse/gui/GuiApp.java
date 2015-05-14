7b9450648c12f5bfe0652ca4f1237f8ea93f3f8a
FUUbi <fabriparrillo@hotmail.com>
2015-05-14 16:00:20 +0200

    contraints verschiedene namen gegeben und button hinzugefügt

--- src/org/fhnw/dataanalyse/gui/GuiApp.java
+++ src/org/fhnw/dataanalyse/gui/GuiApp.java
@@ -45,15 +45,18 @@ public class GuiApp  extends JFrame{
     /*Layout*/
         GridBagLayout distributor = new GridBagLayout();
         GridBagConstraints contraints;
+        GridBagConstraints contraints1;
+        GridBagConstraints contraints2;
         Container surface = getContentPane();
 
         surface.setLayout(distributor);
+
         /*toolbar1 layout */
         contraints = new GridBagConstraints();
         contraints.gridx = 0;
         contraints.gridy = 0;
         contraints.gridwidth = width;
-        contraints.gridheight = 1;
+        contraints.gridheight = 10;
         contraints.fill = GridBagConstraints.HORIZONTAL;
         contraints.anchor = GridBagConstraints.FIRST_LINE_START;
         contraints.weightx =1;
@@ -63,27 +66,30 @@ public class GuiApp  extends JFrame{
         toolbar1.setBackground(Color.blue);
 
         /*toolbar2 layout*/
-        contraints = new GridBagConstraints();
-        contraints.gridx = 0;
-        contraints.gridy = 1;
-        contraints.gridwidth = width;
-        contraints.gridheight = 10;
-        contraints.fill = GridBagConstraints.HORIZONTAL;
-        contraints.anchor = GridBagConstraints.NORTH;
-        distributor.setConstraints(toolbar2, contraints);
+        contraints1 = new GridBagConstraints();
+        contraints1.gridx = 0;
+        contraints1.gridy = 1;
+        contraints1.gridwidth = width;
+        contraints1.gridheight = 10;
+        contraints1.fill = GridBagConstraints.HORIZONTAL;
+        contraints1.anchor = GridBagConstraints.NORTH;
+        distributor.setConstraints(toolbar2, contraints1);
         surface.add(toolbar2);
         toolbar2.setBackground(Color.darkGray);
+        toolbar2.add(new Button());
 
         /*PlotingArea layout*/
-        contraints = new GridBagConstraints();
-        contraints.gridx = 0;
-        contraints.gridy = 2;
-        contraints.gridwidth = width;
-        contraints.fill = GridBagConstraints.BOTH;
-        contraints.gridwidth = GridBagConstraints.REMAINDER;
-        distributor.setConstraints(plotingArea, contraints);
+        contraints2 = new GridBagConstraints();
+        contraints2.gridx = 0;
+        contraints2.gridy = 2;
+        contraints2.gridwidth = width;
+        contraints2.fill = GridBagConstraints.BOTH;
+        contraints2.gridwidth = GridBagConstraints.REMAINDER;
+        distributor.setConstraints(plotingArea, contraints2);
         surface.add(plotingArea);
         plotingArea.setBackground(Color.orange);
+
+        plotingArea.add(new Button());
 
 
         setVisible(true);
