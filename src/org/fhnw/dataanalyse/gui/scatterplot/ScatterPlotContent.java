3bb1ef3e2fa7b5209d36c59ad1d982116cf34be5
Seven71992 <sven.schneider@hotmail.com>
2015-05-20 15:41:00 +0200

    anpassung scatterplot

--- src/org/fhnw/dataanalyse/gui/scatterplot/ScatterPlotContent.java
+++ src/org/fhnw/dataanalyse/gui/scatterplot/ScatterPlotContent.java
@@ -12,14 +12,22 @@ public class ScatterPlotContent extends JPanel {
 
     private final ArrayList<Float> x;
     private final ArrayList<Float> y;
+    private final Float maxX;
+    private final Float maxY;
+    private final Float minY;
 
-    public ScatterPlotContent(ArrayList<Float> x, ArrayList<Float> y) {
+    public ScatterPlotContent(ArrayList<Float> x, ArrayList<Float> y, Float maxX, Float maxY, Float minY) {
         this.x = x;
         this.y = y;
+        this.maxX = maxX;
+        this.maxY = maxY;
+        this.minY = minY;
     }
 
     boolean drawLine = true;
 
+
+    @Override
     protected void paintComponent(Graphics g) {
     int distance = getWidth()/x.size();
         for (int i = 1; i < x.size(); i++) {
@@ -29,10 +37,14 @@ public class ScatterPlotContent extends JPanel {
             double valueY2 = y.get(i);
 
             /*Initialiserung der Koordinatenwerte*/
-            int a = (int) valueX1;
-            int b = (int) (getHeight()-valueY1);
-            int c = (int) valueX2;
-            int d = (int) (getHeight()-valueY2);
+            int a = (int) ((valueX1/maxX)*getWidth());
+            int b = (int) ((valueY1/maxY)*getHeight());
+            //b = b - (getWidth()/2);
+            b = (getHeight() - b);//-(getHeight()/2);
+            int c = (int) ((valueX2/maxX)*getWidth());
+            //c = c-(getWidth()/2);
+            int d = (int) ((valueY2/maxY)*getHeight());
+            d = (getHeight() - d);//-(getHeight()/2);
 
             //de wert muess verändert werde, wenn me e slider für d grössi vo de pünkt macht
             int diameter = 10;
